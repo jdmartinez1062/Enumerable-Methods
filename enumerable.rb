d6b93cc307d46556a9ce921a50a9f82aa066c473
jdmartinez1062 <jd.martinez1062@gmail.com>
2019-10-29 16:43:38 -0500

    finished all 3 methods

--- enumerable.rb
+++ enumerable.rb
@@ -29,4 +29,60 @@ module Enumerable
     end
     array
   end
+  def my_all?(condition = UNDEFINED)
+    check = true
+
+    if block_given?
+      my_each do |x|
+        return false unless yield(x)
+      end
+    elsif condition
+      my_each do |x|
+        return false unless x == condition
+      end
+    else
+      my_each do |x|
+        return false unless x
+      end
+    end
+    check
+  end
+
+  def my_any?(condition = UNDEFINED)
+    check = false
+
+    if block_given?
+      my_each do |x|
+        return true if yield(x)
+      end
+    elsif condition
+      my_each do |x|
+        return true if x == condition
+      end
+    else
+      my_each do |x|
+        return true if x
+      end
+    end
+    check
+  end
+
+  def my_none?
+    check = true
+
+    if block_given?
+      my_each do |x|
+        return false if yield(x)
+      end
+    elsif condition
+      my_each do |x|
+        return false if x == condition
+      end
+    else
+      my_each do |x|
+        return false if x
+      end
+    end
+    check
+  end
 end
