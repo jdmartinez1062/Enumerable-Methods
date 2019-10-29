f33621910708dd141954010783b0d6936919d61f
Your Namejdmartinez1062 <jd.martinez1062@gmail.com>
2019-10-29 15:14:06 -0500

    test

--- enumerable.rb
+++ enumerable.rb
@@ -1,14 +1,12 @@
-# frozen_string_literal: true
-
 module Enumerable
-  UNDEFINED = Object.new
-  def my_each
-    i = 0
-    while i < length
-      yield(self[i])
-      i += 1
+    
+    def my_each 
+        i = 0
+        while i < self.length
+            yield(self[i])
+            i += 1
+        end
     end
-  end
 
-  
+
 end
