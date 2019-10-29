feb47333b4be2075bcb5a9ae2fc22571262efb6f
jdmartinez1062 <jd.martinez1062@gmail.com>
2019-10-29 15:54:57 -0500

    fixed most rubocop issues

--- enumerable.rb
+++ enumerable.rb
@@ -1,25 +1,27 @@
-module Enumerable
-    
-    def my_each 
-        i = 0
-        while i < self.length
-            yield(self[i])
-            i += 1
-        end
-    end
-    def my_each_with_index
-        i = 0
-        while i < length
-          yield(self[i], i)
-          i += 1
-        end
-    end
-    def my_select
-        array = []
-        my_each_with_index do |x, i|
-          array.push(self[i]) if yield(x)
-        end
-        array
-    end
+# frozen_string_literal: true
 
+module Enumerable
+  def my_each
+    i = 0
+    while i < length
+      yield(self[i])
+      i += 1
+    end
+  end
+
+  def my_each_with_index
+    i = 0
+    while i < length
+      yield(self[i], i)
+      i += 1
+    end
+  end
+
+  def my_select
+    array = []
+    my_each_with_index do |x, i|
+      array.push(self[i]) if yield(x)
+    end
+    array
+  end
 end
