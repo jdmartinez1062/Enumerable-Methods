2f9fd8952247f896154ceca64d10b2b4142d7136
Your Namejdmartinez1062 <jd.martinez1062@gmail.com>
2019-10-29 15:07:31 -0500

    rubocop fix to my_each

--- enumerable.rb
+++ enumerable.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 module Enumerable
-    
-    def my_each 
-        i = 0
-        while i < self.length
-            yield(self[i])
-            i += 1
-        end
+  UNDEFINED = Object.new
+  def my_each
+    i = 0
+    while i < length
+      yield(self[i])
+      i += 1
     end
+  end
 
-
+  
 end
